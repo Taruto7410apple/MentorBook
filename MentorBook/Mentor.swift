c8b9d3d91102a4c2ceb83aaeee88c83a87bae86a
TaturoAlmond <64402818+Taruto7410apple@users.noreply.github.com>
2021-02-10 16:20:56 +0900

    Initial Commit

---
+++ MentorBook/Mentor.swift
@@ -0,0 +1,26 @@
+//
+//  Mentor.swift
+//  MentorBook
+//
+//
+//
+
+import UIKit
+
+class Mentor{
+    var name: String!//メンター名
+    var course: String!//担当コース
+    var imageName: String!//顔写真(画像の名前)
+    
+    //初期化
+    init(name:String,imageName:String,course:String){
+        self.name=name
+        self.imageName=imageName
+        self.course=course
+    }
+    
+    func getImage() -> UIImage{
+        return UIImage(named: imageName)!
+    }
+    
+}
